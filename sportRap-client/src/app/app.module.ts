fb0fc4342a163966f96dd4937b12160f56a3175f
Rafael Guzzo <rafaajuda@hotmail.com>
2018-03-23 20:23:10 -0300

    Tela login
    criado componente login e cadastro, criado codigo html e css basico

--- sportRap-client/src/app/app.module.ts
+++ sportRap-client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule
